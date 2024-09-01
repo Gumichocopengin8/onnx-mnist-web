b1e552909695556d50a8c5c0b2fd75acac8d14b9
Keita Nonaka <iKonnyaku40@gmail.com>
2024-09-01 11:56:21 +0900

    build: add web build analyzer

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 import { viteStaticCopy } from 'vite-plugin-static-copy';
+import { visualizer } from 'rollup-plugin-visualizer';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -14,6 +15,7 @@ export default defineConfig({
         },
       ],
     }),
+    visualizer(),
   ],
   server: {
     port: 3000,
