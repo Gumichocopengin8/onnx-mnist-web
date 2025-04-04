3c1c0b8eddbe54997512bdd6d6c147aa745f9c2e
Keita Nonaka <iKonnyaku40@gmail.com>
2025-04-04 09:57:36 -0700

    feat: add loading indicator (#4)

--- web/src/hooks/useResize.ts
+++ web/src/hooks/useResize.ts
@@ -1,6 +1,6 @@
 import { type RefObject, useCallback, useEffect, useState } from 'react';
 
-const useResize = (ref: RefObject<HTMLElement>): [width: number, height: number] => {
+const useResize = (ref: RefObject<HTMLElement | null>): [width: number, height: number] => {
   const [height, setHeight] = useState<number>(0);
   const [width, setWidth] = useState<number>(0);
 
